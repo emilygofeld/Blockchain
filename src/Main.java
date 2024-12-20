bef91274416de2558f350b41d6dd3b2b6e9f2041
emilygofeld <emiloren17@gmail.com>
2024-12-20 21:27:08 +0200

    created initial blockchain, including a block record, block factory, and hashing service

--- src/Main.java
+++ src/Main.java
@@ -18,10 +18,6 @@ public class Main {
         blockchain.add(BlockFactory.generateBlock("Yo im the second block", blockchain.get(blockchain.size()-1).hash()));
         System.out.println("Hash for block 2 : " + blockchain.get(1).hash());
 
-
-        Block second = blockchain.get(1);
-        System.out.println(second.prevHash() + second.timestamp() + second.data());
-
         blockchain.add(BlockFactory.generateBlock("Hey im the third block", blockchain.get(blockchain.size()-1).hash()));
         System.out.println("Hash for block 3 : " + blockchain.get(2).hash());
     }
